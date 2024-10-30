939baeee5160b90a454a719f8d8db2f3e31110aa
Nehakhan8090 <nehakhansawat80@gmail.com>
2024-10-30 06:15:46 -0700

    Add files via upload

---
+++ neha khan.py
@@ -0,0 +1,39 @@
+number1=4
+number2=4
+number3=4
+
+#calculate sum
+sum=number1+number2+number3
+print("sum:",sum )
+
+#calculate difference 
+difference=number1-number2-number3
+print("difference:", difference)
+
+#calculate product
+product=number1*number2*number3
+print ("product:", product)
+
+#calculate average
+average=number1+number2+number3
+print ("average:", average)
+
+#area of triangle
+breadth=9
+height=3
+area=0.5*breadth*height
+print("triangle area",area)
+#area of square
+side=6
+area=side*side
+print("square area", area)
+#area of circle
+pi=3.14
+radius=5
+area=pi*radius*radius
+print("circle area",area)
+#rectangle area 
+length=3
+width=1
+area=length*width
+print("rectangle area",area)
